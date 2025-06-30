0359d903d95cb13422132371b34aa59fab8a9cea
Born-Bohrer <chem1000leaves@gmail.com>
2025-06-30 18:49:32 +0900

    iroiros

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,13 @@
 	const { children } = $props();
 </script>
 
+<svelte:head>
+	<meta property="og:url" content="https://apocalyptic-roomaji.tktb-tess.dev" />
+	<meta property="og:site_name" content="終末式ローマ字" />
+	<meta property="og:image" content="/link-card.jpg" />
+	<meta name="twitter:image" content="/link-card.jpg" />
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:site" content="@triethylamineq" />
+</svelte:head>
+
 {@render children()}
